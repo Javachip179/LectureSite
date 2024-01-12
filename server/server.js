571027b851df48e51e781e4ffe986fbf0c878905
Javachip179 <gher53@naver.com>
2024-01-12 14:24:46 +0900

    Node.js React 연동

--- server/server.js
+++ server/server.js
@@ -14,14 +14,6 @@ const db = mysql.createConnection({
   database: 'coding',
 });
 
-app.get('/', (req, res) => {
-  return res.send('From Client');
-});
-
-app.get('/users', (req, res) => {
-  db.query;
-});
-
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
